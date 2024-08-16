c728b5e867b2130e4c2d7eaa5a0df85181f867db
Aram <schonhagearam@gmail.com>
2024-08-16 14:57:54 +0200

    Added a Second Entity
    * Refactored the JSON Load for the Example Mod to ensure readability

---
+++ TestMods/TestNeoForge/src/main/java/software/bluetest/entity/rex/RexEntity.java
@@ -0,0 +1,108 @@
+package software.bluetest.entity.rex;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.util.GeckoLibUtil;
+import software.bluelib.interfaces.variant.IVariantEntity;
+import software.bluelib.utils.ParameterUtils;
+import software.bluetest.init.ModEntities;
+
+import static software.bluetest.event.ReloadEventHandler.texturesLoader;
+
+public class RexEntity extends TamableAnimal implements IVariantEntity, GeoEntity {
+    // NOTE. 3 Lines Required for the Wiki
+    public static final EntityDataAccessor<String> VARIANT = SynchedEntityData.defineId(RexEntity.class, EntityDataSerializers.STRING);
+
+    public RexEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    // NOTE. Required for the Wiki
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(VARIANT, "");
+    }
+
+    // NOTE. Required for the Wiki
+    @Override
+    public void addAdditionalSaveData(@NotNull CompoundTag pCompound) {
+        super.addAdditionalSaveData(pCompound);
+        pCompound.putString("Variant", getVariantName());
+    }
+
+    // NOTE. Required for the Wiki
+    @Override
+    public void readAdditionalSaveData(@NotNull CompoundTag pCompound) {
+        super.readAdditionalSaveData(pCompound);
+        this.setVariantName(pCompound.getString("Variant"));
+    }
+
+    // NOTE. Required for the Wiki
+    @Override
+    public SpawnGroupData finalizeSpawn(@NotNull ServerLevelAccessor pLevel, @NotNull DifficultyInstance pDifficulty, @NotNull MobSpawnType pReason, @Nullable SpawnGroupData pSpawnData, @Nullable CompoundTag pDataTag) {
+        if (getVariantName().isEmpty()) {
+            System.out.println("Looking for variants at " + getEntityVariants(ModEntities.REX.getKey().location().getPath(), texturesLoader));
+            this.setVariantName(getRandomVariant(getEntityVariants(ModEntities.REX.getKey().location().getPath(), texturesLoader), "normal"));
+            ParameterUtils.ParameterBuilder.forVariant(this.getVariantName())
+                    .withParameter("customParameter")
+                    .withParameter("customParameter2")
+                    .connect();
+            System.out.println("Variant List: " + getEntityVariants(ModEntities.REX.getKey().location().getPath(), texturesLoader));
+            System.out.println("Custom Parameter: " + ParameterUtils.getParameter(this.getVariantName(), "customParameter"));
+        }
+        return super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
+    }
+
+    // NOTE. Required for the Wiki
+    public void setVariantName(String pName) {
+        this.entityData.set(VARIANT, pName);
+    }
+
+    // NOTE. Required for the Wiki
+    @Override
+    public String getVariantName() {
+        return this.entityData.get(VARIANT);
+    }
+
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Mob.createMobAttributes()
+                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.MAX_HEALTH, 10)
+                .add(Attributes.ARMOR, 0)
+                .add(Attributes.ATTACK_DAMAGE, 3)
+                .add(Attributes.FOLLOW_RANGE, 16)
+                .add(Attributes.FLYING_SPEED, 0.3);
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar pControllerRegistrar) {
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(@NotNull ServerLevel pLevel, @NotNull AgeableMob pOtherParent) {
+        return null;
+    }
+}
