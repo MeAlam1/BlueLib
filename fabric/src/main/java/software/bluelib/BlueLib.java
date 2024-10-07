d76537c555afec5e0df99779cc327d69081baa62
Aram <schonhagearam@gmail.com>
2024-10-07 11:52:19 +0200

    Fixed the Welcome Message

--- fabric/src/main/java/software/bluelib/BlueLib.java
+++ fabric/src/main/java/software/bluelib/BlueLib.java
@@ -1,11 +1,19 @@
 package software.bluelib;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class BlueLib implements ModInitializer {
-    
+
+    private boolean hasInitialized = false;
+
     @Override
     public void onInitialize() {
-        BlueLibCommon.init();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (!hasInitialized) {
+                hasInitialized = true;
+                BlueLibCommon.init();
+            }
+        });
     }
 }
