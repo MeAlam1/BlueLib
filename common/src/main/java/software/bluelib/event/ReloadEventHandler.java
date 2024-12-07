fd76c9314a1db806f3ed82d4c0fc0dfe3e732ece
Aram <schonhagearam@gmail.com>
2024-12-07 21:05:51 +0100

    Simplified an double comment

--- common/src/main/java/software/bluelib/event/ReloadEventHandler.java
+++ common/src/main/java/software/bluelib/event/ReloadEventHandler.java
@@ -35,20 +35,6 @@ public class ReloadEventHandler {
      * This method attempts to load variants from both mod and datapack locations. It logs status information and
      * handles exceptions that occur during the loading process.
      * </p>
-     * <p>
-     * Parameters:
-     * <ul>
-     * <li>{@code pFolderPath} {@link String} - The folder path location within the mod or datapack where variants are stored.</li>
-     * <li>{@code pServer} {@link MinecraftServer} - The server instance of the current world.</li>
-     * <li>{@code pModID} {@link String} - The mod ID used to locate the entity variant resources. (Use your Mod's ID)</li>
-     * <li>{@code pEntityName} {@link String} - The entity name to load.</li>
-     * </ul>
-     * <p>
-     * Exception Handling:
-     * <ul>
-     * <li>{@link JsonParseException} - Thrown when there is an error parsing the JSON files.</li>
-     * <li>{@link RuntimeException} - Thrown for unexpected errors during the registration process.</li>
-     * </ul>
      *
      * @param pFolderPath {@link String} - The folder path location within the mod or datapack where variants are stored.
      * @param pServer     {@link MinecraftServer} - The server instance of the current world.
