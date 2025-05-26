1cb226959d67fef397b300a8b6d0617911de6133
Aram <schonhagearam@gmail.com>
2025-05-26 16:20:35 +0200

    /log Works on Fabric Now

--- fabric/src/main/java/software/bluelib/BlueLib.java
+++ fabric/src/main/java/software/bluelib/BlueLib.java
@@ -10,11 +10,13 @@ package software.bluelib;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import software.bluelib.config.ConfigLoader;
 import software.bluelib.event.ChatHandler;
+import software.bluelib.event.CommandHandler;
 import software.bluelib.event.ReloadHandler;
 import software.bluelib.example.event.VariantProvider;
 import software.bluelib.net.FabricNetworkManager;
@@ -46,5 +48,6 @@ public class BlueLib implements ModInitializer {
         ServerLifecycleEvents.END_DATA_PACK_RELOAD.register(ConfigLoader::reloadConfigs);
         ServerLifecycleEvents.END_DATA_PACK_RELOAD.register(ReloadHandler::onReload);
         ServerMessageEvents.ALLOW_CHAT_MESSAGE.register(ChatHandler::onAllowChat);
+        CommandRegistrationCallback.EVENT.register(CommandHandler::registerCommands);
     }
 }
---
+++ fabric/src/main/java/software/bluelib/event/CommandHandler.java
@@ -0,0 +1,14 @@
+package software.bluelib.event;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import software.bluelib.registry.BlueCommandRegistry;
+
+public class CommandHandler {
+	public static void registerCommands(CommandDispatcher<CommandSourceStack> pCommandSourceStackCommandDispatcher, CommandBuildContext pCommandBuildContext, Commands.CommandSelection pCommandSelection) {
+		BlueCommandRegistry.registerCommands(pCommandSourceStackCommandDispatcher);
+	}
+
+}
