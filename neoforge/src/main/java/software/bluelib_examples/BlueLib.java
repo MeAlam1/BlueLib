3a2e44b41208d786eef38445540b5443dfd38b37
Aram <schonhagearam@gmail.com>
2025-05-30 18:17:31 +0200

    Fix

--- neoforge/src/main/java/software/bluelib_examples/BlueLib.java
+++ neoforge/src/main/java/software/bluelib_examples/BlueLib.java
@@ -2,10 +2,14 @@ package software.bluelib_examples;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import software.bluelib.client.BlueLibClient;
 import software.bluelib.event.ReloadHandler;
 import software.bluelib_examples.registry.EntityRegistry;
 import software.bluelib_examples.variant.VariantProvider;
@@ -15,10 +19,13 @@ public class BlueLib {
 
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, BlueLibConstants.MOD_ID);
 
-    public BlueLib(IEventBus pModEventBus) {
+    public BlueLib(IEventBus pModEventBus, ModContainer pModContainer) {
         ReloadHandler.registerProvider(new VariantProvider());
         ENTITIES.register(pModEventBus);
         pModEventBus.<EntityAttributeCreationEvent>addListener(event -> EntityRegistry.registerEntityAttributes(event::put));
+
+        if (FMLEnvironment.dist == Dist.CLIENT)
+            BlueLibClient.init(pModContainer);
 
         BlueLibCommon.doServerRegistration();
     }
--- neoforge/src/main/java/software/bluelib_examples/BlueLibClient.java
+++ neoforge/src/main/java/software/bluelib_examples/BlueLibClient.java
@@ -3,6 +3,7 @@ package software.bluelib_examples;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import software.bluelib_examples.client.BlueLibCommonClient;
@@ -10,9 +11,12 @@ import software.bluelib_examples.client.BlueLibCommonClient;
 @EventBusSubscriber(modid = BlueLibConstants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class BlueLibClient {
 
+    public static void init() {
+        BlueLibCommon.doClientRegistration();
+    }
+    
     @SubscribeEvent
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
         BlueLibCommonClient.registerRenderers(event::registerEntityRenderer, event::registerBlockEntityRenderer);
-        BlueLibCommon.doClientRegistration();
     }
 }
