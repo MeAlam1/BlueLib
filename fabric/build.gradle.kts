7312eb4017dd5f06ac2c994ca0738092f805b120
Aram <schonhagearam@gmail.com>
2025-06-09 00:47:20 +0200

    Fixed Fabric Build

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     compileOnly(project(":common"))
     include(libs.molang)
     modCompileOnly(libs.molang)
+    modCompileOnlyApi(libs.jei.api)
     modRuntimeOnly(libs.jei.fabric)
 
     // ExampleMod
