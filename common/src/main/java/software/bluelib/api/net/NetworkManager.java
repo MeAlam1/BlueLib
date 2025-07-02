f08aff2a70fad7a796f276c636eaedb574d92ecc
Aram <schonhagearam@gmail.com>
2025-07-02 02:08:57 +0200

    license

--- common/src/main/java/software/bluelib/api/net/NetworkManager.java
+++ common/src/main/java/software/bluelib/api/net/NetworkManager.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2024 BlueLib Contributors
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the MIT License was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT.
+ */
 package software.bluelib.api.net;
 
 import net.minecraft.server.level.ServerPlayer;
