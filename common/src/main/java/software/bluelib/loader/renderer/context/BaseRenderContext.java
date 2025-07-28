09d217704f88464da0056adc186dbba6c2ec2bc4
Aram <schonhagearam@gmail.com>
2025-07-28 17:30:59 +0200

    Made the Contexts Mutateble!

--- common/src/main/java/software/bluelib/loader/renderer/context/BaseRenderContext.java
+++ common/src/main/java/software/bluelib/loader/renderer/context/BaseRenderContext.java
@@ -12,13 +12,102 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import software.bluelib.loader.animatable.BlueAnimatable;
 import software.bluelib.loader.cache.model.ModelCache;
 
-public record BaseRenderContext<T extends BlueAnimatable>(
-		PoseStack poseStack,
-		T animatable,
-		ModelCache model,
-		MultiBufferSource bufferSource,
-		boolean isReRender,
-		float partialTick,
-		int packedLight,
-		int packedOverlay,
-		int color) implements IRenderContext<T> {}
+@SuppressWarnings("unused")
+public final class BaseRenderContext<T extends BlueAnimatable> implements IRenderContext<T> {
+
+	private PoseStack poseStack;
+	private T animatable;
+	private ModelCache model;
+	private MultiBufferSource bufferSource;
+	private boolean isReRender;
+	private float partialTick;
+	private int packedLight;
+	private int packedOverlay;
+	private int color;
+
+	public BaseRenderContext(PoseStack pPoseStack, T pAnimatable, ModelCache pModel,
+			MultiBufferSource pBufferSource, boolean pIsReRender, float pPartialTick,
+			int pPackedLight, int pPackedOverlay, int pColor) {
+		this.poseStack = pPoseStack;
+		this.animatable = pAnimatable;
+		this.model = pModel;
+		this.bufferSource = pBufferSource;
+		this.isReRender = pIsReRender;
+		this.partialTick = pPartialTick;
+		this.packedLight = pPackedLight;
+		this.packedOverlay = pPackedOverlay;
+		this.color = pColor;
+	}
+
+	public PoseStack poseStack() {
+		return poseStack;
+	}
+
+	public T animatable() {
+		return animatable;
+	}
+
+	public ModelCache model() {
+		return model;
+	}
+
+	public MultiBufferSource bufferSource() {
+		return bufferSource;
+	}
+
+	public boolean isReRender() {
+		return isReRender;
+	}
+
+	public float partialTick() {
+		return partialTick;
+	}
+
+	public int packedLight() {
+		return packedLight;
+	}
+
+	public int packedOverlay() {
+		return packedOverlay;
+	}
+
+	public int color() {
+		return color;
+	}
+
+	public void setPoseStack(PoseStack pPoseStack) {
+		this.poseStack = pPoseStack;
+	}
+
+	public void setAnimatable(T pAnimatable) {
+		this.animatable = pAnimatable;
+	}
+
+	public void setModel(ModelCache pModel) {
+		this.model = pModel;
+	}
+
+	public void setBufferSource(MultiBufferSource pBufferSource) {
+		this.bufferSource = pBufferSource;
+	}
+
+	public void setReRender(boolean pReRender) {
+		isReRender = pReRender;
+	}
+
+	public void setPartialTick(float pPartialTick) {
+		this.partialTick = pPartialTick;
+	}
+
+	public void setPackedLight(int pPackedLight) {
+		this.packedLight = pPackedLight;
+	}
+
+	public void setPackedOverlay(int pPackedOverlay) {
+		this.packedOverlay = pPackedOverlay;
+	}
+
+	public void setColor(int pColor) {
+		this.color = pColor;
+	}
+}
--- common/src/main/java/software/bluelib/loader/renderer/context/FullRenderContext.java
+++ common/src/main/java/software/bluelib/loader/renderer/context/FullRenderContext.java
@@ -14,15 +14,123 @@ import net.minecraft.client.renderer.RenderType;
 import software.bluelib.loader.animatable.BlueAnimatable;
 import software.bluelib.loader.cache.model.ModelCache;
 
-public record FullRenderContext<T extends BlueAnimatable>(
-		PoseStack poseStack,
-		T animatable,
-		ModelCache model,
-		RenderType renderType,
-		MultiBufferSource bufferSource,
-		VertexConsumer buffer,
-		boolean isReRender,
-		float partialTick,
-		int packedLight,
-		int packedOverlay,
-		int color) implements IRenderContext<T> {}
+@SuppressWarnings("unused")
+public final class FullRenderContext<T extends BlueAnimatable> implements IRenderContext<T> {
+
+	private PoseStack poseStack;
+	private T animatable;
+	private ModelCache model;
+	private RenderType renderType;
+	private MultiBufferSource bufferSource;
+	private VertexConsumer buffer;
+	private boolean isReRender;
+	private float partialTick;
+	private int packedLight;
+	private int packedOverlay;
+	private int color;
+
+	public FullRenderContext(PoseStack pPoseStack, T pAnimatable, ModelCache pModel,
+			RenderType pRenderType, MultiBufferSource pBufferSource,
+			VertexConsumer pBuffer, boolean pIsReRender, float pPartialTick,
+			int pPackedLight, int pPackedOverlay, int pColor) {
+		this.poseStack = pPoseStack;
+		this.animatable = pAnimatable;
+		this.model = pModel;
+		this.renderType = pRenderType;
+		this.bufferSource = pBufferSource;
+		this.buffer = pBuffer;
+		this.isReRender = pIsReRender;
+		this.partialTick = pPartialTick;
+		this.packedLight = pPackedLight;
+		this.packedOverlay = pPackedOverlay;
+		this.color = pColor;
+	}
+
+	public PoseStack poseStack() {
+		return poseStack;
+	}
+
+	public T animatable() {
+		return animatable;
+	}
+
+	public ModelCache model() {
+		return model;
+	}
+
+	public RenderType renderType() {
+		return renderType;
+	}
+
+	public MultiBufferSource bufferSource() {
+		return bufferSource;
+	}
+
+	public VertexConsumer buffer() {
+		return buffer;
+	}
+
+	public boolean isReRender() {
+		return isReRender;
+	}
+
+	public float partialTick() {
+		return partialTick;
+	}
+
+	public int packedLight() {
+		return packedLight;
+	}
+
+	public int packedOverlay() {
+		return packedOverlay;
+	}
+
+	public int color() {
+		return color;
+	}
+
+	public void setPoseStack(PoseStack pPoseStack) {
+		this.poseStack = pPoseStack;
+	}
+
+	public void setAnimatable(T pAnimatable) {
+		this.animatable = pAnimatable;
+	}
+
+	public void setModel(ModelCache pModel) {
+		this.model = pModel;
+	}
+
+	public void setRenderType(RenderType pRenderType) {
+		this.renderType = pRenderType;
+	}
+
+	public void setBufferSource(MultiBufferSource pBufferSource) {
+		this.bufferSource = pBufferSource;
+	}
+
+	public void setBuffer(VertexConsumer pBuffer) {
+		this.buffer = pBuffer;
+	}
+
+	public void setReRender(boolean pReRender) {
+		this.isReRender = pReRender;
+	}
+
+	public void setPartialTick(float pPartialTick) {
+		this.partialTick = pPartialTick;
+	}
+
+	public void setPackedLight(int pPackedLight) {
+		this.packedLight = pPackedLight;
+	}
+
+	public void setPackedOverlay(int pPackedOverlay) {
+		this.packedOverlay = pPackedOverlay;
+	}
+
+	public void setColor(int pColor) {
+		this.color = pColor;
+	}
+}
