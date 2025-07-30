0ff76e5a57599a8cf0bfae6e49eab0130547a0b3
Aram <schonhagearam@gmail.com>
2025-07-30 21:46:18 +0200

    2.3.0 Release

--- common/src/main/java/software/bluelib/BlueLibConstants.java
+++ common/src/main/java/software/bluelib/BlueLibConstants.java
@@ -49,7 +49,7 @@ public class BlueLibConstants implements BuildDetails {
 	public static final String MOD_NAME = "BlueLib";
 
 	@NotNull
-	public static final String VERSION = "2.2.0";
+	public static final String VERSION = "2.3.0";
 
 	@Nullable
 	public static MinecraftServer server;
