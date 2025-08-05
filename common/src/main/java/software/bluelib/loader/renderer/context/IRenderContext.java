5babb2b4effc8188b78324a26c6a6ab6fd0c2be5
Aram <schonhagearam@gmail.com>
2025-08-05 16:14:51 +0200

    Done

--- common/src/main/java/software/bluelib/loader/renderer/context/IRenderContext.java
+++ common/src/main/java/software/bluelib/loader/renderer/context/IRenderContext.java
@@ -12,8 +12,6 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import software.bluelib.loader.animatable.BlueAnimatable;
 import software.bluelib.loader.cache.model.ModelCache;
 
-// TODO: Change all the BlueRenderer (And Inheritors) Methods to use this RenderContext instead of passing all the parameters individually.
-
 public sealed interface IRenderContext<T extends BlueAnimatable>
 		permits BaseRenderContext, FullRenderContext {
 
