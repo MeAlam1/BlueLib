9aa1c350bea2799f4711d70e44ab6aba8cc83fd5
MeAlam1 <schonhagearam@gmail.com>
2025-12-30 15:30:55 +0100

    2.4.1 Release

--- common/src/main/java/software/bluelib/BlueLibConstants.java
+++ common/src/main/java/software/bluelib/BlueLibConstants.java
@@ -52,7 +52,7 @@ public class BlueLibConstants implements BuildDetails {
 	public static final String MOD_NAME = "BlueLib";
 
 	@NotNull
-	public static final String VERSION = "2.4.0";
+	public static final String VERSION = "2.4.1";
 
 	@Nullable
 	public static MinecraftServer server;
